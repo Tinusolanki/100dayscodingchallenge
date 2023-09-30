50c1dba4168750d0314401519b7f755625be6786
Tinusolanki <145601431+Tinusolanki@users.noreply.github.com>
2023-09-30 22:48:42 +0530

    Add files via upload

---
+++ day9_ordar.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class Order{
+public static void main (String args[])
+{
+ Scanner sc= new Scanner( System.in);
+ System.out.println( " please Enter the 1st number =>  ");
+ int input1=sc.nextInt();
+System.out.println( " please Enter the 2nd number =>  ");
+ int input2=sc.nextInt();
+System.out.println( " please Enter the 3rd number =>  ");
+ int input3=sc.nextInt();
+ if (input1<input2  &&  input2<input3)
+{
+ System.out.println("its  Increasing order ");
+}
+else if( input1<input2  &&   input2>input3 )
+{
+  System.out.println(" its decreasing order ");
+}
+else
+{
+System.out.println(" its not order");
+}
+}
+}
+
+
