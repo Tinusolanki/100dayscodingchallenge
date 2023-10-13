b9c3baac9c0c0163ef78e91eb8c56c4591eba10c
Tinusolanki <145601431+Tinusolanki@users.noreply.github.com>
2023-10-13 20:06:15 +0530

    Add files via upload

---
+++ Day17_FiveTabel.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class TABEL{
+public static void main(String args[])
+{
+	int num = 5;
+	for (int i =1; i<=10; ++i)
+	{
+		System.out.printf("%d * %d = %d \n", num, i, num*i);
+	}
+           }
+}
