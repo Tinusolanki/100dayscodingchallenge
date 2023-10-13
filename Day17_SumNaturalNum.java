4a8b6e3f9eff4aab4733a6427caabea61c513d6f
Tinusolanki <145601431+Tinusolanki@users.noreply.github.com>
2023-10-13 20:06:44 +0530

    Add files via upload

---
+++ Day17_SumNaturalNum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class SUMNATURALNUM{
+public static void main(String []args)
+{
+	int i,num = 5 , sum = 0;
+	for(i=1; i<=num; ++i)
+	{
+		sum = sum+i;
+	}
+System.out.println("Sum of First 5 Natural Number is =" +sum);
+	}
+}
